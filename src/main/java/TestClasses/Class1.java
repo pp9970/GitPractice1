a178eead71a2ef9f423b001ff1157525db2f4e8e
PRATIK BHOSLE <PRATIK BHOSLE@LAPTOP-3TPPPFOH>
2022-08-01 10:14:59 +0530

    1st time commiting

---
+++ src/main/java/TestClasses/Class1.java
@@ -0,0 +1,31 @@
+package TestClasses;
+
+public class Class1 
+{
+	public static void method1()
+	{
+		System.out.println("method1");
+	}
+
+	public static void method2()
+	{
+		System.out.println("method2");
+	}
+	public static void method3()
+	{
+		System.out.println("method3");
+	}
+
+	
+	
+	
+	public static void main(String[] args)
+	{
+		method1();
+		
+	}
+	
+	
+	
+	
+}
